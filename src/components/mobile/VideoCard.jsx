02f4e16c354e14ebc262b7aab62aada30014102d
AMK1215 <aungmyokhaing1215@gmail.com>
2025-09-16 10:30:28 +0630

    first init

---
+++ src/components/mobile/VideoCard.jsx
@@ -0,0 +1,25 @@
+import { forwardRef } from 'react';
+
+const VideoCard = forwardRef(({ video }, ref) => {
+  return (
+    <div
+      className='videocard'
+      style={{
+        height: '80vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <video
+        ref={ref}
+        className='videocard__video'
+        src={'https://ponewine22x.xyz/api/..' + video}
+        loop
+        style={{ width: '100%', height: '100%' }}
+      />
+    </div>
+  );
+});
+
+export default VideoCard;
---
+++ src/hooks/baseUrl.jsx
@@ -0,0 +1,4 @@
+// const BASE_URL = 'https://delightmyanmarthb.xyz/api';
+const BASE_URL = 'https://ponewine22x.xyz/api';
+
+export default BASE_URL;
